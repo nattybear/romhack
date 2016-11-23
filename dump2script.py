391c70e5e5905541a38a4bb41c6b9d17fda1083e
Vincent <nattybear@naver.com>
2016-11-24 00:02:54 +0900

    제어코드 enter 추가, 스크립트 덤프 방식 변경. replace -> 있는 것만 변환하고 모르는 코드는 16진수로 변환

--- dump2script.py
+++ dump2script.py
@@ -1,37 +1,42 @@
 from sys import argv, exit
-from struct import pack
+from struct import unpack, error
+from binascii import hexlify
 
 if len(argv) != 3:
-    print '[*] Usage: python dump2script.py <dump> <csv>'
-    exit()
-    
+  print '[*] Usage: python dump2script.py <dump> <csv>'
+  exit()
+
 dump = argv[1]
 csv = argv[2]
 
-csv_fp = open(csv, 'rb')
-csv_buf = csv_fp.read()
-csv_fp.close()
+fp = open(csv, 'rb')
+buf = fp.read()
+fp.close()
+list = buf.split(b'\x0d\x0a')
 
-csv_list = csv_buf.split(b'\x0d\x0a')
-csv_dic = {}
-
-for a, b in enumerate(csv_list):
-    csv_dic[a] = b
-
-dump_fp = open(dump, 'rb')
-
-new = dump.split('.')[0] + '.txt'
-new_fp = open(new, 'wb')
+fp = open(dump, 'rb')
+buf1 = ''
 
 while True:
-    try:
-        dump_buf = dump_fp.read(2)
-        num = unpack('H', dump_buf)[0]
-        if num in csv_dic.keys():
-            new_fp.write(dump_buf)
-    except:
-        break
-        
-new_fp.close()
+  buf = fp.read(2)
+  try:
+    num = unpack('H', buf)[0]
+    if num == 65535:
+      buf1 = buf1 + b'\x0d\x0a'
+      continue
+  except error:
+    break
+
+  try:
+    buf1 = buf1 + list[num]
+  except IndexError:
+    buf1 = buf1 + '{' + hexlify(buf) + '}'
+
+fp.close()
+
+new = dump.split('.')[0] + '.txt'
+fp = open(new, 'wb')
+fp.write(buf1)
+fp.close()
 
 print '[*]', new, 'saved'
