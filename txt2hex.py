242e4111dbfd5e93716174ad31fa73db4c488455
Vincent <nattybear@naver.com>
2016-11-24 17:40:22 +0900

    there is an error. it never stop yet

--- txt2hex.py
+++ txt2hex.py
@@ -1,6 +1,28 @@
 from sys import argv, exit
+from struct import pack
+from binascii import hexlify
 
-if len(argv) != 2:
-    print '[*] Usage: python txt2hex.py <txt>'
+if len(argv) != 3:
+    print '[*] Usage: python txt2hex.py <txt> <csv>'
     exit()
 
+txt = argv[1]
+csv = argv[2]
+
+fp1 = open(txt, 'rb')
+
+fp2 = open(csv, 'rb')
+buf2 = fp2.read()
+fp2.close()
+
+list = buf2.split(b'\x0d\x0a')
+
+fp1.seek(3)
+
+while True:
+    buf1 = fp1.read(3)
+    for a, b in enumerate(list):
+        if buf1 == b:
+            print hexlify(pack('H', a))
+        else:
+            continue
