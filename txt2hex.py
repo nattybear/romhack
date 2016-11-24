ba3be52ae2069350cef1693c2580ac1c99b869c3
Vincent <nattybear@naver.com>
2016-11-24 17:55:03 +0900

    use future print function

--- txt2hex.py
+++ txt2hex.py
@@ -1,9 +1,10 @@
+from __future__ import print_function
 from sys import argv, exit
 from struct import pack
 from binascii import hexlify
 
 if len(argv) != 3:
-    print '[*] Usage: python txt2hex.py <txt> <csv>'
+    print('[*] Usage: python txt2hex.py <txt> <csv>')
     exit()
 
 txt = argv[1]
@@ -23,7 +24,7 @@ while True:
     buf1 = fp1.read(3)
     for a, b in enumerate(list):
         if buf1 == b:
-            print hexlify(pack('H', a)),
+            print(hexlify(pack('H', a)), end='')
         else:
             continue
     if len(buf1) == 0:
