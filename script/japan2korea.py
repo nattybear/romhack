a5cd48fe611099c9f5c4d202fd5dc5c4edb690b5
Vincent <nattybear@naver.com>
2016-12-29 13:04:08 +0900

    일어 헥사값을 한글 헥사값으로 변경

---
+++ script/japan2korea.py
@@ -0,0 +1,39 @@
+from sys import argv, exit
+
+if len(argv) != 4:
+  print 'Usage:', argv[0], '<rom>', '<japan>', '<korea>'
+  exit()
+
+rom = argv[1]
+japan = argv[2]
+korea = argv[3]
+new = 'out_' + rom
+
+fp1 = open(rom, 'rb')
+fp2 = open(japan, 'rb')
+fp3 = open(korea, 'rb')
+fp4 = open(new, 'wb')
+
+buf1 = fp1.read()
+buf2 = fp2.read()
+buf3 = fp3.read()
+buf4 = buf1
+
+list2 = buf2.split(b'\x0d\x0a')
+list3 = buf3.split(b'\x0d\x0a')
+
+size2 = len(list2)
+size3 = len(list3)
+
+if size2 != size3: print '[*] size is not equal'; exit()
+
+for i in range(size2):
+  buf4 = buf4.replace(list2[i], list3[i])
+
+fp4.write(buf4)
+
+len1 = len(buf1)
+len4 = len(buf4)
+
+print '[*]', new, 'saved'
+print '[*] %d bytes changed' % (len4 - len1)
